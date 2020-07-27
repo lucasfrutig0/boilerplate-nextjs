aea965291d2d98d85af725b4690752dc287953cc
lucasfrutig0 <lucasfrutig0@gmail.com>
2020-07-27 16:35:40 -0300

    config storyblook

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,10 @@ describe('<Main />', () => {
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
   })
+
+  it('should render the colors correctyly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
